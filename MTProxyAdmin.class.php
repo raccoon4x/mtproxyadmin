f4184747a3c1c46e83e9922bd9202be1de117d1d
raccoon4x <raccoon4x@gmail.com>
2019-02-22 23:50:03 +0300

    Установка промоканала от одного админа
    По команде /setpromo @channel или /setpromo t.me/channel

--- MTProxyAdmin.class.php
+++ MTProxyAdmin.class.php
@@ -5,9 +5,12 @@ class MTProxyAdmin extends \danog\MadelineProto\EventHandler
 
     const DRPROXYBOT = 1626995;
     const MTPROXYBOT = 571465504;
+    const TAG = '0fbfccfcc2290628c7fefd7030f9f4a7'; // TAG от которого требуется менять пароль
 
     private $stack = [];
     private $lock = false;
+
+    private $setchannel = [];
 
     private $message;
 
@@ -45,29 +48,12 @@ class MTProxyAdmin extends \danog\MadelineProto\EventHandler
             $this->message = $update['message']['message'];
         }
         if ($update['message']['from_id'] === self::MTPROXYBOT){
-            if ($this->need_stat !== null) {
-                if(strpos($update['message']['message'], 'Promoted channel:')!==false){
-                    $bytes = $this->findProxyButtonByHash($update, 'Stats');
-                    if($bytes) {
-                        $botCallbackAnswer = $this->messages->getBotCallbackAnswer(
-                            [
-                                'game' => false,
-                                'peer' => self::MTPROXYBOT,
-                                'msg_id' => $update['message']['id'],
-                                'data' => base64_decode($bytes),
-                            ]
-                        );
-                    }
-                }
-                if(strpos($update['message']['message'], 'Stats for proxy with tag')!==false
-                || strpos($update['message']['message'], 'Sorry, we don\'t have stats for your proxy yet.')!==false){
-                    $this->forwardMessage(self::DRPROXYBOT, $update['message']['from_id'], [$update['message']['id']]);
-                    $this->need_stat = null;
-                }
-            }
-            if(is_array($this->setchannel)){
+            if(!empty($this->setchannel)){
+                $this->setPromo($update);
                 if(strpos($update['message']['message'], 'Promoted channel:')!==false) {
-                    $bytes = $this->findProxyButtonByHash($update, 'Set promotion');
+                    $bytes = strpos($update['message']['message'], 'Promoted channel: n/a.') !== false ?
+                        $this->findProxyButtonByHash($update, 'Set promotion') :
+                        $this->findProxyButtonByHash($update, 'Edit promotion');
                     if($bytes) {
                         $botCallbackAnswer = $this->messages->getBotCallbackAnswer(
                             [
@@ -110,139 +96,22 @@ class MTProxyAdmin extends \danog\MadelineProto\EventHandler
 
     private function onUpdateDRProxyBot($update)
     {
-        $this->addTask($update['message']['message']);
-        if($this->assertText('/wipe')){
-            unset($this->stack);
-            $this->disableLock();
-        }
-        \danog\MadelineProto\Logger::log($this->stack);
-        if($this->hasTasks()){
-            $this->enableLock();
-            if($this->getCurrentCommand() === 'newproxy'){
-                if($this->getCurrentStep() === 0){
-                    $this->sendMessage(self::MTPROXYBOT, '/newproxy');
-                    $this->incStep();
-                }
-            }
-            if (in_array($this->getCurrentCommand(), ['stat', 'setpromo'])){
-                if($this->getCurrentStep() === 0){
-                    $this->sendMessage(self::MTPROXYBOT, '/myproxies');
-                    $this->incStep();
-                }
-            }
-        }else{
-            $this->disableLock();
+        if(preg_match('/\/setpromo\s+(?:.*\/)?(?:\@)?(?<channel>\w+)/', $update['message']['message'], $match)){
+            $this->setchannel['channel'] = $match['channel'];
+            $this->sendMessage(self::MTPROXYBOT, '/myproxies');
         }
     }
 
     private function newMessageMTProxyBot($update)
     {
-        if($this->hasTasks()){
-            $this->enableLock();
-            if($this->assertText('Sorry, too many attempts.')){
-                $answer = ['to'=>$this->getCurrentTask()['from'], 'message'=>'Sorry, too many attempts. Please try again later.'];
-                $answer = json_encode($answer);
-                $this->sendMessage(self::DRPROXYBOT, $answer);
-                $this->deleteCurrentTask();
-            }
-            if($this->getCurrentCommand() === 'newproxy'){
-                if($this->getCurrentStep() === 1){
-                    if($this->assertText('Registering a new proxy server.')){
-                        $message = $this->getCurrentTask()['server'].':'.$this->getCurrentTask()['port'];
-                        $this->sendMessage(self::MTPROXYBOT, $message);
-                        $this->incStep();
-                    }
-                }
-                if($this->getCurrentStep() === 2){
-                    if($this->assertText('Now please specify its secret in hex format')){
-                        $message = $this->getCurrentTask()['secret'];
-                        $this->sendMessage(self::MTPROXYBOT, $message);
-                        $this->incStep();
-                    }
-                }
-                if($this->getCurrentStep() === 3){
-                    if($this->assertText('Your proxy has been successfully registered.')){
-                        $message = $this->message;
-                        $answer = ['to'=>$this->getCurrentTask()['from'], 'message'=>$message];
-                        $answer = json_encode($answer);
-                        $this->sendMessage(self::DRPROXYBOT, $answer);
-                        $this->deleteCurrentTask();
-                    }
-                    if($this->assertText('Incorrect secret value.')){
-                        $message = $this->message;
-                        $answer = ['to'=>$this->getCurrentTask()['from'], 'message'=>$message];
-                        $answer = json_encode($answer);
-                        $this->sendMessage(self::DRPROXYBOT, $answer);
-                        $this->deleteCurrentTask();
-                    }
-                }
-            }
-        }
-
-
-
-        if (is_array($this->newproxy)) {
-            if ($this->isCommand($update['message']['message'], 'Registering a new proxy server.')) {
-                $message = $this->newproxy['server'] . ':' . $this->newproxy['port'];
-                $this->sendMessage(self::MTPROXYBOT, $message);
-            }
-            if ($this->isCommand($update['message']['message'], 'Now please specify its secret in hex format')) {
-                $message = $this->newproxy['secret'];
-                $this->sendMessage(self::MTPROXYBOT, $message);
-            }
-            if ($this->isCommand($update['message']['message'], 'Success!')) {
-                $message = 'Новые прокси успешно зарегистрированы.'.PHP_EOL;
-                if(preg_match('/software\syou\sare\susing\:\s(?<hash>.*?)\./', $update['message']['message'], $match)){
-                    $message.= 'Хэш: '.$match['hash'];
-                }else{
-                    $message.= 'Ошибка при получении хэша проксей.';
-                }
-                $this->sendMessage(self::DRPROXYBOT, $message, $this->last_msg_id);
-                $this->newproxy = null;
-                $this->last_msg_id = null;
-            }
-        }
-        if($this->need_stat!==null){
-            if(strpos($update['message']['message'], 'Here is the list of all proxies you created:', 0)===0){
-                $bytes = $this->findProxyButtonByHash($update, $this->need_stat);
-                if($bytes){
-                    $botCallbackAnswer = $this->messages->getBotCallbackAnswer(
-                        [
-                            'game' => false,
-                            'peer' => self::MTPROXYBOT,
-                            'msg_id' => $update['message']['id'],
-                            'data' => base64_decode($bytes),
-                        ]
-                    );
-                }else{
-                    $this->sendMessage(self::DRPROXYBOT, 'Не найдено', $this->last_msg_id);
-                    $this->need_stat = null;
-                }
-            }
-        }
-        if(is_array($this->setchannel)){
-            if(strpos($update['message']['message'], 'Here is the list of all proxies you created:', 0)===0){
-                $bytes = $this->findProxyButtonByHash($update, $this->setchannel['hash']);
-                if($bytes){
-                    $botCallbackAnswer = $this->messages->getBotCallbackAnswer(
-                        [
-                            'game' => false,
-                            'peer' => self::MTPROXYBOT,
-                            'msg_id' => $update['message']['id'],
-                            'data' => base64_decode($bytes),
-                        ]
-                    );
-                }else{
-                    $this->sendMessage(self::DRPROXYBOT, 'Не найдено', $this->last_msg_id);
-                    $this->setchannel = null;
-                }
-            }
-            if(strpos($update['message']['message'], 'Here is the list of all proxies you created:', 0)===0) {
-                $this->sendMessage(self::MTPROXYBOT, $this->setchannel['channel'], $update['message']['id']);
+        if(!empty($this->setchannel)){
+            $this->setPromo($update);
+            if(strpos($update['message']['message'], 'This allows you to set up a promoted channel for your proxy', 0)===0) {
+                $this->sendMessage(self::MTPROXYBOT, '@'.$this->setchannel['channel'], $update['message']['id']);
             }
             if(strpos($update['message']['message'], 'New promoted channel has been set', 0)===0){
                 $this->forwardMessage(self::DRPROXYBOT, $update['message']['from_id'], [$update['message']['id']]);
-                $this->setchannel = null;
+                $this->setchannel = [];
             }
         }
     }
@@ -313,12 +182,33 @@ class MTProxyAdmin extends \danog\MadelineProto\EventHandler
         $rows = $update['message']['reply_markup']['rows'];
         foreach ($rows as $key){
             foreach ($key['buttons'] as $button){
-                if(strpos($button['text'], $hash) !== false){
+                if(strpos($button['text'], $hash) !== false || strpos($button['text'], '»') !== false){
+                    // Если нашли кнопку с нужным текстом, то кликаем на нее, в случае, если есть кнопка листать дальше, листаем
                     return $button['data']['bytes'];
                 }
             }
         }
         return false;
+    }
+
+    private function setPromo($update)
+    {
+        if(strpos($update['message']['message'], 'Here is the list of all proxies you created:', 0)===0){
+            $bytes = $this->findProxyButtonByHash($update, self::TAG);
+            if($bytes){
+                $botCallbackAnswer = $this->messages->getBotCallbackAnswer(
+                    [
+                        'game' => false,
+                        'peer' => self::MTPROXYBOT,
+                        'msg_id' => $update['message']['id'],
+                        'data' => base64_decode($bytes),
+                    ]
+                );
+            }else{
+                $this->sendMessage(self::DRPROXYBOT, 'Can\'t find tag '.self::TAG);
+                $this->setchannel = [];
+            }
+        }
     }
 
     /**
